08c7de4cd87a757787b58018a5e176faa0675acb
tatitati <francis.albusac@simplybusiness.co.uk>
2020-11-25 20:15:58 +0000

    more shit

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const { app, BrowserWindow, Notification } = require('electron');
+const path = require('path');
 
 function createWindow(){
     const win = new BrowserWindow({
         width: 800,
         height: 900,
+        backgroundColor: "white",
         webPreferences: {
             nodeIntegration: true
         }
     })
 
     win.loadFile("index.html")
+    win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
@@ -19,5 +22,7 @@ app.whenReady().then(() => {
             "body": "my body"
         })
         notification.show()
+        const parsed = path.parse('/Users/francis.albusac/lab/mylab/electron_react_playground')
+        console.log(parsed)
     }    
 )
