737a497dc992249de7bfa3275c25cb8bb20dbb3d
tatitati <francis.albusac@simplybusiness.co.uk>
2020-11-25 19:56:10 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 800,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile("index.html")
+}
+
+app.whenReady().then(createWindow)
